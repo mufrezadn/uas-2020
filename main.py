aeb327b23fc8e7b65d69a762cfee616e56ea5b73
mufrezadn <57052783+mufrezadn@users.noreply.github.com>
2020-01-18 19:06:07 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from core.baseapp import BaseApp
+
+
+class MainApp(BaseApp):
+    def __init__(self):
+        self.books = []
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
+class view(ViewBook):
+    def __init__(self):
+        vBook = ViewBook(self.book)
+        vBook.list()
+
+if __nane__=="__main__":
+    App = Main()
+    App.run()
